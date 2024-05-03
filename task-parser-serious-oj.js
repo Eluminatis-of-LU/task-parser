e3a06e2b2b2396b4e72af7deadf5f061fe877232
Araf Al Jami <11315571+ajami1331@users.noreply.github.com>
2024-05-03 18:09:45 +0100

    Create task-parser-serious-oj.js

---
+++ task-parser-serious-oj.js
@@ -0,0 +1,48 @@
+// ==UserScript==
+// @name         task-parser-serious-oj
+// @namespace    http://tampermonkey.net/
+// @version      2024-05-03
+// @description  try to take over the world!
+// @author       Araf Al Jami
+// @match        https://judge.eluminatis-of-lu.com/p/*
+// @match        https://judge.eluminatis-of-lu.com/contest/*/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=eluminatis-of-lu.com
+// @grant        none
+// ==/UserScript==
+
+window.parse_task = () =>
+{
+     const titleElement = document.getElementsByClassName("location-current")[0];
+     const taskName = titleElement.innerText;
+     const timeLimitElement = document.getElementById("time_limit");
+     const memoryLimitElement = document.getElementById("memory_limit");
+     const timeLimitStr = timeLimitElement.innerText;
+     const memoryLimitStr = memoryLimitElement.innerText;
+     const inputElements = document.getElementsByClassName("test-case-input");
+     const outputElements = document.getElementsByClassName("test-case-output");
+     const inputs = [...inputElements].map(el => el.innerText.slice(0, -2));
+     const outputs = [...outputElements].map(el => el.innerText.slice(0, -2));
+     console.log('taskName:', taskName);
+     console.log('timeLimitStr:', timeLimitStr);
+     console.log('memoryLimitStr:', memoryLimitStr);
+     console.table(inputs);
+     console.table(outputs);
+     const task = {
+         'name': taskName,
+         'tests': []
+     }
+     for (let i = 0; i < inputs.length; i++) {
+         task['tests'].push({
+             'input': inputs[i],
+             'output': outputs[i],
+         })
+     }
+     console.log(task);
+     fetch('http://localhost:4244/', { method: 'POST', body: JSON.stringify(task) })
+}
+
+(function() {
+    'use strict';
+     const titleElement = document.getElementsByClassName("section__title")[0];
+     titleElement.outerHTML += `<button onclick="parse_task()" class="primary button">Parse Task</button>`
+})();
