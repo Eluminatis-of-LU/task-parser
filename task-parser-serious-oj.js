e27b1003f5878fd664610df13fa8740a73b3bf9c
Araf Al Jami <11315571+ajami1331@users.noreply.github.com>
2024-05-03 18:22:51 +0100

    Update task-parser-serious-oj.js

--- task-parser-serious-oj.js
+++ task-parser-serious-oj.js
@@ -2,7 +2,7 @@
 // @name         task-parser-serious-oj
 // @namespace    http://tampermonkey.net/
 // @version      2024-05-03
-// @description  parse task from serious-oj
+// @description  try to take over the world!
 // @author       Araf Al Jami
 // @match        https://judge.eluminatis-of-lu.com/p/*
 // @match        https://judge.eluminatis-of-lu.com/contest/*/*
@@ -20,8 +20,8 @@ window.parse_task = () =>
      const memoryLimitStr = memoryLimitElement.innerText;
      const inputElements = document.getElementsByClassName("test-case-input");
      const outputElements = document.getElementsByClassName("test-case-output");
-     const inputs = [...inputElements].map(el => el.innerText.slice(0, -6));
-     const outputs = [...outputElements].map(el => el.innerText.slice(0, -6));
+     const inputs = [...inputElements].map(el => el.innerText.slice(0, -7));
+     const outputs = [...outputElements].map(el => el.innerText.slice(0, -7));
      console.log('taskName:', taskName);
      console.log('timeLimitStr:', timeLimitStr);
      console.log('memoryLimitStr:', memoryLimitStr);
